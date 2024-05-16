31da04b1ddd72dc7b100bc2320bb146a8720e144
John Tasler <taz@mysticpop.com>
2024-05-15 17:00:00 -0700

    feat: add version info and string resources.

---
+++ cpp/GetConsoleInfo/resource.h
@@ -0,0 +1,17 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by GetConsoleInfo.rc
+//
+#define IDS_STRING_ELEVATED_PREFIX_DEFAULT 101
+#define IDS_ELEVATED_PREFIX_DEFAULT     101
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
