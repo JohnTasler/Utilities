58e655cf5b7bea578316a4d3eb83da44eed5b1d7
John Tasler <taz@mysticpop.com>
2025-07-02 02:39:01 -0700

    update with newer Interop stuff

--- cs/ListROT/Program.cs
+++ cs/ListROT/Program.cs
@@ -1,49 +1,44 @@
-﻿using System;
 using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
 using Tasler.Interop.Com;
 
-namespace ListROT
+namespace ListROT;
+
+class Program
 {
-	class Program
+	[MTAThread]
+	static void Main(string[] args)
 	{
-		[MTAThread]
-		static void Main(string[] args)
+		using (var rot = new RunningObjectTable())
 		{
-			using (var rot = new RunningObjectTable())
+			var bindCtx = ComApi.CreateBindCtx();
+
+			var monikers = rot.ToArray();
+			Console.WriteLine("{0} Items in the Running Object Table:", monikers.Length);
+
+			var maxDigits = (int)Math.Truncate(Math.Log10(monikers.Length)) + 1;
+			var digitsFormatString = "D" + maxDigits;
+
+			var index = 0;
+			var displayName = string.Empty;
+			var displayIndex = string.Empty;
+			foreach (var moniker in monikers)
 			{
-				using (var bindCtx = new ComPtr<IBindCtx>(ComApi.CreateBindCtx()))
+				using (new ComPtr<IMoniker>(moniker))
 				{
-					var monikers = rot.ToArray();
-					Console.WriteLine("{0} Items in the Running Object Table:", monikers.Length);
+					++index;
+					displayIndex = index.ToString(digitsFormatString);
 
-					var maxDigits = (int)Math.Truncate(Math.Log10(monikers.Length)) + 1;
-					var digitsFormatString = "D" + maxDigits;
-
-					var index = 0;
-					var displayName = string.Empty;
-					var displayIndex = string.Empty;
-					foreach (var moniker in monikers)
-					{
-						using (new ComPtr<IMoniker>(moniker))
-						{
-							++index;
-							displayIndex = index.ToString(digitsFormatString);
-
-							moniker.GetDisplayName(bindCtx.Value, null, out displayName);
-						}
-
-						Console.WriteLine("  [{0}] {1}", displayIndex, displayName);
-					}
+					displayName = moniker.GetDisplayName(bindCtx, null);
 				}
 
-				if (Debugger.IsAttached)
-				{
-					Console.Write("Press any key to continue . . . ");
-					Console.ReadKey(true);
-				}
+				Console.WriteLine("  [{0}] {1}", displayIndex, displayName);
 			}
+		}
+
+		if (Debugger.IsAttached)
+		{
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
 		}
 	}
 }
